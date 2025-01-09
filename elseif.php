d94bfc27c5c121dfa532a35bc6301fd8ea6620f0
YourName <youremail@example.com>
2025-01-09 16:46:55 +0600

    first commit

---
+++ elseif.php
@@ -0,0 +1,23 @@
+<?php
+    $shamiul = 25;
+    $akbor = 27;
+
+    if ($shamiul > $akbor){
+        echo "Shamiul";
+    }else if($akbor> $shamiul){
+        echo "Akbor ";
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
